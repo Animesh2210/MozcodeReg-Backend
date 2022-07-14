fa039a75baf832937448df06137c7cfe1b9c8287
Animesh Verma <animeshverma2210@gmail.com>
2022-07-14 14:43:36 +0530

    Sql intigration done

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-import json
-from unicodedata import name
-import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import sqlalchemy
+import databases
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+DATABASE_URL = "postgresql://ugnlqudyvozqly:4d99c0cedc5a9dc2981f03252e471c33a97d9f8acdbedf4f93daeb27b44e013a@ec2-23-23-182-238.compute-1.amazonaws.com:5432/dbp5bm3o21pc8a"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+people_data = sqlalchemy.Table(
+    "participants",
+    metadata,
+    sqlalchemy.Column("phone", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("email", sqlalchemy.String),
+    sqlalchemy.Column("college", sqlalchemy.String),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+
+# class NoteIn(BaseModel):
+#     text: str
+#     completed: bool
+
+
+# class Note(BaseModel):
+#     id: int
+#     text: str
+#     completed: bool
 
 class ppl(BaseModel):
     name: str
@@ -11,11 +41,24 @@ class ppl(BaseModel):
     email: str
     name = phone = college = email = ""
 
+
 app = FastAPI()
 
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 @app.post("/signup/")
-def signup(details:ppl):
-    return details.name
+async def signup(details:ppl):
+    query = people_data.insert().values(phone=details.phone,name=details.name,email=details.email,college=details.college)
+    await database.execute(query)
+    return {"Status":True}
 
 @app.get("/")
 def wel():
